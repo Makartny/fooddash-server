b10f86815dc13351fd4e65a376467bdca4259470
Kostyn <kostyan@example.com>
2026-08-20 22:15:01 +0300

    feat: add Product POST endpoint and create json and http api test drafts

--- src/main/java/com/kostyan/fooddash/controller/ProductController.java
+++ src/main/java/com/kostyan/fooddash/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.kostyan.fooddash.controller;
 import com.kostyan.fooddash.model.Product;
 import com.kostyan.fooddash.repository.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +24,17 @@ public class ProductController {
     public List<Product> getAllProducts() {
         // Просто берём из репозитория ВСЕ блюда и возвращаем их прямо в сеть!
         return productRepository.findAll();
+    }
+
+    // ==========================================
+    // ВХОДЯЩИЕ ВОРОТА: ПРИЕМ И СОХРАНЕНИЕ НОВОГО БЛЮДА
+    // ==========================================
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product newProduct) {
+        // Убедись, что 'newProduct' тут и ниже написаны ОДИНАКОВО (с большой буквы P в середине)!
+        System.out.println("📥 На сервер прилетел новый продукт: " + newProduct.getName());
+
+        return productRepository.save(newProduct);
     }
 
 
